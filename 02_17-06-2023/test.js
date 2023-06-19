33afb50895cfb8cefedef7c15c3b25bbd9b7e771
ManishGoti15 <121253758+ManishGoti15@users.noreply.github.com>
2023-06-19 18:41:04 +0530

    Add files via upload

---
+++ 02_17-06-2023/test.js
@@ -0,0 +1,11 @@
+// console.log("test ----->");
+// document.getElementById("print").innerHTML = "Meet ";
+
+let a = 15;
+let b = 12;
+
+let arr = []
+
+console.log("total ----->", a + b);
+
+// console.log("value1 :", value1);
